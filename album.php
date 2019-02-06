e560368c104a1650d8611c1db741adde0b453a11
lootfee <lutfiayven@gmail.com>
2019-02-07 03:33:50 +0400

    added song and album class

--- album.php
+++ album.php
@@ -9,13 +9,65 @@ else {
 }
 
 $album = new Album($con, $albumId);
-
 $artist = $album->getArtist();
-
-echo $album->getTitle() . "<br>";
-echo $artist->getName();
-
+#echo $album->getTitle() . "<br>";
+#echo $artist->getName();
 ?>
+
+<div class="entityInfo">
+
+	<div class="leftSection">
+		<img src="<?php echo $album->getArtworkPath() ;?>" alt="Album image">
+	</div>
+
+	<div class="rightSection">
+		<h2><?php echo $album->getTitle(); ?></h2>
+		<span><?php echo $artist->getName(); ?></span>	
+		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+	</div>
+
+</div>
+
+<div class="trackListContainer">
+	<ul class="trackList">
+		
+		<?php 
+		$songIdArray = $album->getSongIds();
+
+		$i = 1;
+		foreach($songIdArray as $songId) {
+			
+			$albumSong = new Song($con, $songId);
+			$albumArtist = $albumSong->getArtist();
+
+			#echo $albumSong->getTitle();
+			echo "<li class='trackListRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/icons/play.png'>
+						<span class='trackNumber'>$i</span>
+					</div>
+
+					<div class='trackInfo'>
+						<span class='trackName'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					</div>
+
+					<div class='trackOptions'>
+						<img src='optionsButton' src='assets/icons/more.png'>
+					</div>
+
+					<div class='trackDuration'>
+						<span class='duration'>" . $albumSong->getDuration() . "</span>
+					</div>
+
+					</li>";
+			
+					$i = $i + 1;
+			}
+		 ?>
+		
+	</ul>
+</div>
 
 
 
